3d5fd1b1719899cb0282fa881c8b1113487088bb
Zandri2k <sandrasiphokaziii@gmail.com>
2024-02-05 14:19:23 +0100

    my first plot with matplotlib

---
+++ code-alongs/first_plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+
+numbers =  list (range (10))
+
+squares = [number**2 for number in numbers]
+
+print(squares)
+print (numbers)
+
+plt.plot(numbers, squares)
+plt.title("x^2 for positive intergers, 0-9")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
